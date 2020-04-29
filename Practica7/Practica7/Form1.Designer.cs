d3e278acbc8e1ea3650dd1af7128ef4d3d185dfb
silviasalazar <47604124+silviasalazar@users.noreply.github.com>
2020-04-28 23:53:40 -0700

    Add files via upload

---
+++ Practica7/Practica7/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Practica7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMultihilos = new System.Windows.Forms.Button();
+            this.progress1 = new System.Windows.Forms.ProgressBar();
+            this.progress2 = new System.Windows.Forms.ProgressBar();
+            this.progress3 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSecuencial = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.progressBar4 = new System.Windows.Forms.ProgressBar();
+            this.progressBar5 = new System.Windows.Forms.ProgressBar();
+            this.progressBar6 = new System.Windows.Forms.ProgressBar();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.btnLimpiar1 = new System.Windows.Forms.Button();
+            this.btnLimpiar2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMultihilos
+            // 
+            this.btnMultihilos.Location = new System.Drawing.Point(49, 41);
+            this.btnMultihilos.Name = "btnMultihilos";
+            this.btnMultihilos.Size = new System.Drawing.Size(75, 23);
+            this.btnMultihilos.TabIndex = 0;
+            this.btnMultihilos.Text = "Ejecutar";
+            this.btnMultihilos.UseVisualStyleBackColor = true;
+            this.btnMultihilos.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // progress1
+            // 
+            this.progress1.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.progress1.Location = new System.Drawing.Point(179, 10);
+            this.progress1.Name = "progress1";
+            this.progress1.Size = new System.Drawing.Size(175, 23);
+            this.progress1.TabIndex = 1;
+            // 
+            // progress2
+            // 
+            this.progress2.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.progress2.Location = new System.Drawing.Point(371, 11);
+            this.progress2.Name = "progress2";
+            this.progress2.Size = new System.Drawing.Size(175, 22);
+            this.progress2.TabIndex = 2;
+            // 
+            // progress3
+            // 
+            this.progress3.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.progress3.Location = new System.Drawing.Point(581, 10);
+            this.progress3.Name = "progress3";
+            this.progress3.Size = new System.Drawing.Size(175, 21);
+            this.progress3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Multihilos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Secuencial";
+            // 
+            // btnSecuencial
+            // 
+            this.btnSecuencial.Location = new System.Drawing.Point(49, 277);
+            this.btnSecuencial.Name = "btnSecuencial";
+            this.btnSecuencial.Size = new System.Drawing.Size(75, 23);
+            this.btnSecuencial.TabIndex = 6;
+            this.btnSecuencial.Text = "Ejecutar";
+            this.btnSecuencial.UseVisualStyleBackColor = true;
+            this.btnSecuencial.Click += new System.EventHandler(this.btnSecuencial_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(179, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(175, 162);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(371, 43);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(175, 162);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(581, 43);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(175, 162);
+            this.textBox3.TabIndex = 9;
+            // 
+            // progressBar4
+            // 
+            this.progressBar4.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.progressBar4.Location = new System.Drawing.Point(179, 248);
+            this.progressBar4.Name = "progressBar4";
+            this.progressBar4.Size = new System.Drawing.Size(175, 23);
+            this.progressBar4.TabIndex = 10;
+            // 
+            // progressBar5
+            // 
+            this.progressBar5.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.progressBar5.Location = new System.Drawing.Point(371, 248);
+            this.progressBar5.Name = "progressBar5";
+            this.progressBar5.Size = new System.Drawing.Size(175, 23);
+            this.progressBar5.TabIndex = 11;
+            // 
+            // progressBar6
+            // 
+            this.progressBar6.BackColor = System.Drawing.Color.DarkViolet;
+            this.progressBar6.Location = new System.Drawing.Point(581, 248);
+            this.progressBar6.Name = "progressBar6";
+            this.progressBar6.Size = new System.Drawing.Size(175, 23);
+            this.progressBar6.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(179, 277);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(175, 162);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(371, 277);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(175, 162);
+            this.textBox5.TabIndex = 14;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(581, 277);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(175, 162);
+            this.textBox6.TabIndex = 15;
+            // 
+            // btnLimpiar1
+            // 
+            this.btnLimpiar1.Location = new System.Drawing.Point(49, 71);
+            this.btnLimpiar1.Name = "btnLimpiar1";
+            this.btnLimpiar1.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar1.TabIndex = 16;
+            this.btnLimpiar1.Text = "Limpiar";
+            this.btnLimpiar1.UseVisualStyleBackColor = true;
+            this.btnLimpiar1.Click += new System.EventHandler(this.btnLimpiar1_Click);
+            // 
+            // btnLimpiar2
+            // 
+            this.btnLimpiar2.Location = new System.Drawing.Point(49, 306);
+            this.btnLimpiar2.Name = "btnLimpiar2";
+            this.btnLimpiar2.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar2.TabIndex = 17;
+            this.btnLimpiar2.Text = "Limpiar";
+            this.btnLimpiar2.UseVisualStyleBackColor = true;
+            this.btnLimpiar2.Click += new System.EventHandler(this.btnLimpiar2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Violet;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpiar2);
+            this.Controls.Add(this.btnLimpiar1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.progressBar6);
+            this.Controls.Add(this.progressBar5);
+            this.Controls.Add(this.progressBar4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSecuencial);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progress3);
+            this.Controls.Add(this.progress2);
+            this.Controls.Add(this.progress1);
+            this.Controls.Add(this.btnMultihilos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMultihilos;
+        private System.Windows.Forms.ProgressBar progress1;
+        private System.Windows.Forms.ProgressBar progress2;
+        private System.Windows.Forms.ProgressBar progress3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSecuencial;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ProgressBar progressBar4;
+        private System.Windows.Forms.ProgressBar progressBar5;
+        private System.Windows.Forms.ProgressBar progressBar6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnLimpiar1;
+        private System.Windows.Forms.Button btnLimpiar2;
+    }
+}
+
---
+++ Practica7/Practica7/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Practica7
+{
+    public partial class Form1 : Form
+    {
+        private TaskScheduler _secuencial;
+        
+        
+        //Se crea el hilo
+        Thread hilo;
+        //Actualizar Progress bar
+        delegate void delegado(int valor);
+        public Form1()
+        {
+            InitializeComponent();
+            _secuencial = TaskScheduler.FromCurrentSynchronizationContext();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            
+            hilo = new Thread(new ThreadStart(Progreso1));
+            hilo.Start();
+            hilo = new Thread(new ThreadStart(Progreso2));
+            hilo.Start();
+            hilo = new Thread(new ThreadStart(Progreso3));
+            hilo.Start();
+
+        }
+        
+        //Incrementantan valoren es las barras
+        public void Progreso1()
+        {
+            
+            for(int i=0;i<=100;i++)
+            {
+                Delegate MD = new delegado(Actualizar1);
+                this.Invoke(MD, new object[] { i });
+                Thread.Sleep(50);
+            }
+            CheckForIllegalCrossThreadCalls = false;
+            
+            //Imprimir numeros del 1 al 1000
+            for(int i=0;i<=1000;i++)
+            {
+                textBox1.Text += i.ToString()+ Environment.NewLine;
+            }
+          
+
+            
+        }
+      
+        //Actualiza barra de progreso
+        public void Actualizar1(int valor)
+        {
+            progress1.Value = valor;
+        }
+       
+
+        public void Progreso2()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Delegate MD = new delegado(Actualizar2);
+                this.Invoke(MD, new object[] { i });
+                Thread.Sleep(40);
+            }
+            CheckForIllegalCrossThreadCalls = false;
+
+            //Genera 100 numeros aleatorios
+            Random random = new Random();
+            for(int i=0;i<=100;i++)
+            {
+                
+               textBox2.Text += (random.Next(0, 11)).ToString()+ Environment.NewLine;
+            }
+
+        }
+        public void Actualizar2(int valor)
+        {
+            progress2.Value = valor;
+        }
+        public void Progreso3()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Delegate MD = new delegado(Actualizar3);
+                this.Invoke(MD, new object[] { i });
+                Thread.Sleep(30);
+            }
+            CheckForIllegalCrossThreadCalls = false;
+            //calcular media
+            string cadena1 = "56782469104569875698452";
+            double suma = 0.0;
+            for (int i = 0; i < cadena1.Length; i++)
+            {
+                suma = suma + Convert.ToInt32(cadena1.Substring(i, 1));
+            }
+            double resultado = suma / cadena1.Length;
+
+
+            textBox3.Text = "La media aritmetica es: " + resultado.ToString();
+
+        }
+        public void Actualizar3(int valor)
+        {
+            progress3.Value = valor;
+        }
+
+        private void btnSecuencial_Click(object sender, EventArgs e)
+        {
+            new Task(() =>
+            {
+                Task[] tareas = {
+                Task.Factory.StartNew(() => IncreaseValue(textBox4, progressBar4, 100,1), CancellationToken.None, TaskCreationOptions.LongRunning, _secuencial),
+                Task.Factory.StartNew(() => IncreaseValue(textBox5, progressBar5, 100, 1), CancellationToken.None, TaskCreationOptions.LongRunning, _secuencial),
+                Task.Factory.StartNew(() => IncreaseValue(textBox6, progressBar6, 100, 1), CancellationToken.None, TaskCreationOptions.LongRunning, _secuencial),
+                
+                };
+                Task.WaitAll(tareas);
+
+            }).Start();
+
+          
+            
+
+        }
+        private void IncreaseValue(TextBox t, ProgressBar p, int r, int c)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            p.Maximum = r;
+             for (int i = 0; i <= r; i++)
+             {
+                 int result = i *c;
+
+                 Thread.Sleep(10);
+
+                 UpdateView(t, p, result, c);
+             }
+            
+
+            //Imprimir numeros del 1 al 1000
+            for (int i = 0; i <= 1000; i++)
+            {
+                t.Text += i.ToString() + Environment.NewLine;
+            }
+            
+
+            //Genera 100 numeros aleatorios
+            Random random = new Random();
+            for (int i = 0; i <= 100; i++)
+            {
+
+                t.Text += (random.Next(0, 11)).ToString() + Environment.NewLine;
+            }
+
+
+            //calcular media
+            string cadena1 = "56782469104569875698452";
+            double suma = 0.0;
+            for (int i = 0; i < cadena1.Length; i++)
+            {
+                suma = suma + Convert.ToInt32(cadena1.Substring(i, 1));
+            }
+            double resultado = suma / cadena1.Length;
+
+
+            t.Text = "La media aritmetica es: " + resultado.ToString();
+
+
+        }
+
+        private void UpdateView(TextBox t, ProgressBar p, int result, int c)
+        {
+            p.Value = result;
+          
+            //t.Text += "Count: " + result + Environment.NewLine;
+            t.SelectionStart = t.Text.Length;
+            t.ScrollToCaret();
+            Application.DoEvents();
+        }
+
+        private void btnLimpiar1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            progress1.Value = 0;
+            progress2.Value = 0;
+            progress3.Value = 0;
+        }
+
+        private void btnLimpiar2_Click(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            progressBar4.Value = 0;
+            progressBar5.Value = 0;
+            progressBar6.Value = 0;
+        }
+    }
+}
